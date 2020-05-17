aed4ca84349ff219616ccbad4b236506ba6fb795
Allan Assis <allan.andrade@g.globo>
2020-05-16 21:02:27 -0300

    First project commit

---
+++ background.js
@@ -0,0 +1,89 @@
+
+var state = new State()
+
+chrome.browserAction.onClicked.addListener(() => {
+  state.toggle()
+  return true
+})
+
+chrome.tabs.onActivated.addListener(({tabId}) => {
+  if(tabId in state.get()){
+    state.activeCurrent(tabId)
+  } else {
+    state.reset()
+  }
+    return true
+})
+
+chrome.tabs.onRemoved.addListener((tabId, {windowId, isClosing}) => {
+  if(isClosing){
+    state.flushall()
+  }
+  else{
+    state.flush(tabId)
+  }
+})
+
+function State(){
+  var activeState = {
+    icon: "active.png",
+    active: true
+  }
+
+  var inactiveState = {
+    icon: "inactive.png",
+    active: false
+  }
+
+  var state = {
+    current: inactiveState
+  }
+
+  var setState = function(iconPath, active){
+    iconConfig = { path: iconPath }
+
+    chrome.browserAction.setIcon(iconConfig)
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      var tabId = tabs[0].id
+      state[tabId] = state.current
+      chrome.tabs.sendMessage(tabId, { active });
+  
+    });
+  }
+
+  this.activeCurrent = function(tabId){
+    state.current = state[tabId]
+    setState(state.current.icon, state.current.active)
+  }
+
+  this.active = function(){
+    state.current = activeState
+    setState(state.current.icon, state.current.active)
+  }
+
+  this.inactive = function(){
+    state.current = inactiveState
+    setState(state.current.icon, state.current.active)
+  }
+
+  this.toggle = function(){
+    state.current = state.current.active ? inactiveState : activeState
+    setState(state.current.icon, state.current.active)
+  }
+
+  this.flushall = function(){
+    state = {current: inactiveState}
+  }
+
+  this.flush = function(tabId){
+    delete state[tabId]
+  }
+
+  this.reset = function(){
+    this.inactive()
+  }
+
+  this.get = function(){
+    return state
+  }
+}
---
+++ content.js
@@ -0,0 +1,52 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log("aqui meu mermao")
+        console.log("oi?", request.oi)
+        if(request.active){
+            console.log("Ativando...")
+            start()
+        } else if (!request.active){
+            console.log("Desativando...")
+            stop()
+        }
+    }
+);
+
+function start() {
+    var tags = document.querySelectorAll("*")
+    tags.forEach(tag => {
+        tag.addEventListener("click", handleTag)
+    })
+
+}
+
+function stop() {
+    var tags = document.querySelectorAll("*")
+    tags.forEach(tag => {
+        tag.removeEventListener("click", handleTag)
+    })
+}
+
+function handleTag(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    switch (e.path[0].tagName.toLowerCase()) {
+        case "img":
+            window.open(e.toElement.src)
+            break;
+        case "image":
+            window.open(e.toElement.href.baseVal)
+        default:
+            var img = e.toElement.parentElement.querySelector("img")
+            var image = e.toElement.parentElement.querySelector("image")
+            if(img){
+                window.open(img.src)
+            }
+            else if(image){
+                window.open(image.href.baseVal)
+            }
+            
+            break;
+    }
+}
